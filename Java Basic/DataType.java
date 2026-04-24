39d57bb65769902567f8ab7d5730d227dd6ecc37
Omkumar9506 <hari.9506563662@gmail.com>
2026-04-24 19:43:25 +0530

    dataType

---
+++ Java Basic/DataType.java
@@ -0,0 +1,21 @@
+public class DataType {
+
+    public static void main(String[] args) {
+        byte num1 = 100;
+        System.out.println(num1);
+        short num2 = 150;
+        System.out.println(num2);
+        int num3 = 54665;
+        System.out.println(num3);
+        long num4 = 556216546;
+        System.out.println(num4);
+        float pi = 3.14f;
+        System.out.println(pi);
+        double number = 5665423365.5;
+        System.out.println(number);
+        char letter = 'H';
+        System.out.println(letter);
+        boolean isTrue = false;
+        System.out.println(isTrue);
+    }
+}
